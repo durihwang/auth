bb1c631fafe812e3846ec9aefbb3996d20a70ba0
황두리 <hdr@gabia.com>
2021-03-10 17:50:34 +0900

    GCMT-10 health check controller 추가

--- src/main/java/com/gabia/auth/aop/OAuth2CacheAop.java
+++ src/main/java/com/gabia/auth/aop/OAuth2CacheAop.java
@@ -2,6 +2,7 @@ package com.gabia.auth.aop;
 
 import com.gabia.auth.service.CacheService;
 import lombok.val;
+import net.minidev.json.JSONObject;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,7 +12,11 @@ import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.Optional;
 
 @Component
@@ -30,34 +35,64 @@ public class OAuth2CacheAop {
     @Around("execution(* org.springframework.security.oauth2.provider.client.JdbcClientDetailsService.loadClientByClientId(..))")
     public Object loadClientByClientId(ProceedingJoinPoint pjp) throws Throwable {
 
-        Object proceed = pjp.proceed();
-        cacheService.save(proceed, "client");
-//        findCache("client");
-        return proceed;
+        Object client = findCache("client");
+        if (client instanceof Object) {
+            return client;
+        } else {
+            Object proceed = pjp.proceed();
+            cacheService.save(proceed, "client");
+            System.out.println("no Cache");
+            return proceed;
+        }
     }
 
-    @Around("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.postAccessToken(..))")
+    /*@Around("execution(* org.springframework.security.oauth2.provider.endpoint.TokenEndpoint.postAccessToken(..))")
     public Object postAccessToken(ProceedingJoinPoint pjp) throws Throwable {
         ResponseEntity proceed = (ResponseEntity) pjp.proceed();
         cacheService.save(proceed, "token");
         return proceed;
     }
 
-    @Around("execution(* org.springframework.security.oauth2.provider.token.TokenStore.readAccessToken(..))")
+    @Around("execution(* org.springframework.security.oauth2.provider.token.store.JdbcTokenStore.readAccessToken(..))")
     public Object readAccessToken(ProceedingJoinPoint pjp) throws Throwable {
 
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        System.out.println("request = " + getHeaders(request));
+
         Object proceed = pjp.proceed();
-        /*Object token = findCache("token");
-        DefaultOAuth2AccessToken defaultOAuth2AccessToken = new DefaultOAuth2AccessToken((String) token);*/
+//        System.out.println("proceed = " + proceed);
+        *//*Object token = findCache("token");
+        DefaultOAuth2AccessToken defaultOAuth2AccessToken = new DefaultOAuth2AccessToken((String) token);*//*
         return proceed;
     }
 
-    private Object findCache(String name) throws Throwable {
+    @Around("execution(* org.springframework.jdbc.core.JdbcTemplate.queryForObject(..))")
+    public Object queryForObject(ProceedingJoinPoint pjp) throws Throwable {
+        Object proceed = pjp.proceed();
+        System.out.println("proceed = " + proceed);
+        return proceed;
+    }
 
-        Cache cache = redisCacheManager.getCache("oauth");
-        Object getCache = cache.get(name).get();
-        System.out.println("getCache = " + getCache);
+    private static JSONObject getHeaders(HttpServletRequest request) {
+        JSONObject jsonObject = new JSONObject();
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String param = headers.nextElement();
+            String replaceParam = param.replaceAll("\\.", "-");
+            jsonObject.put(replaceParam, request.getHeader(param));
+        }
+        return jsonObject;
+    }*/
 
-        return getCache;
+    private Object findCache(String name) {
+
+        Optional<CacheManager> cache = Optional.ofNullable(redisCacheManager);
+        Object oauth = cache.orElse(null).getCache("oauth").get(name);
+
+        if (oauth instanceof Object) {
+            return ((Cache.ValueWrapper) oauth).get();
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/main/java/com/gabia/auth/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.gabia.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    public String checkHealth(){
+        return "OK";
+    }
+
+}
