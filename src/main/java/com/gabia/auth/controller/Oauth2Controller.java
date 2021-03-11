19bd46b2a02165d9d3e95b13930673efe0a8f379
황두리 <hdr@gabia.com>
2021-03-11 17:05:10 +0900

    GCMT-10 kms 설정 관련 추가

--- src/main/java/com/gabia/auth/controller/Oauth2Controller.java
+++ src/main/java/com/gabia/auth/controller/Oauth2Controller.java
@@ -1,13 +1,12 @@
 package com.gabia.auth.controller;
 
 import com.gabia.auth.dto.Oauth;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class Oauth2Controller {
 
     @RequestMapping("/oauth/validation")
