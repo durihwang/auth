74c68020e4366632f759042ae56a2e26dbbfdf3a
황두리 <hdr@gabia.com>
2021-03-18 17:03:09 +0900

    GCMT-10 Oauth2Controller POST 설정

--- src/main/java/com/gabia/auth/controller/Oauth2Controller.java
+++ src/main/java/com/gabia/auth/controller/Oauth2Controller.java
@@ -4,12 +4,13 @@ import com.gabia.auth.dto.Oauth;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Oauth2Controller {
 
-    @RequestMapping("/oauth/validation")
+    @RequestMapping(value = "/oauth/validation", method = RequestMethod.POST)
     public ResponseEntity<Oauth> Validation() {
         Oauth oauth = new Oauth();
         oauth.setMsg("Success");
