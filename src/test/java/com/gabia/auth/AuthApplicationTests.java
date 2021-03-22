6cca97168a6df4c4fb41bdbdd0a0bb2438d54f12
황두리 <hdr@gabia.com>
2021-03-22 15:18:21 +0900

    GCMT-10 test code 수정

--- src/test/java/com/gabia/auth/AuthApplicationTests.java
+++ src/test/java/com/gabia/auth/AuthApplicationTests.java
@@ -4,6 +4,7 @@ import com.gabia.auth.entity.TokenEntity;
 import com.gabia.auth.repositories.TokenRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -18,9 +19,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Profile("local")
+@Profile("test")
 @SpringBootTest(
-        args = {"--spring.profiles.active=local"
+        args = {"--spring.profiles.active=test"
                 , "--hiworks.kms.role-id=a2d67753-929c-14b9-ccac-5b5086141e68"
                 , "--hiworks.kms.secret-id=42a39ca7-fa56-5ec0-f085-71ce90e5d6bd"
                 , "--hiworks.kms.engine-name=gabia"}
